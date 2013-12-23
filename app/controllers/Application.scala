c5208ae96440ad27c0d2a01850b2ca6ac05e09a9
John Bessire <johnb10000@yahoo.com>
2013-12-23 15:12:28 -0800

    Added instruction to use WebSockets on Heroku

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,13 @@ import play.api.libs.iteratee.Concurrent
 import play.api.libs.iteratee.Iteratee
 import play.api.libs.concurrent.Execution.Implicits._
 
+// HEROKU WebSockets setup
+//    If you get heroku error 501: not supported then add WebSockets to Heroku
+//    by typing the following Heroku command.
+//
+//        heroku labs:enable websockets -a the-heroku-application-name
+//   
+
 object Application extends Controller {
        
 	  def index =  WebSocket.using[String] { request =>
